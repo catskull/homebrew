522e38df19e8c1bb5d6f2fe7ce6769eee9b8feb1
David DeGraw <degraw@fastmail.com>
2025-02-28 12:56:27 -0700

    Create openemu-arm.rb

---
+++ Casks/openemu-arm.rb
@@ -0,0 +1,44 @@
+cask "openemu-arm" do
+  version "2.4.1-ARM"
+  sha256 :no_check # Replace with actual SHA if available for security
+
+  url "https://github.com/Azyzraissi/OpenEmu/releases/download/2.4.1-ARM/OpenEmu-2.4.1-ARM64.zip"
+  name "OpenEmu ARM"
+  desc "Retro video game emulation for macOS (ARM build)"
+  homepage "https://github.com/Azyzraissi/OpenEmu"
+
+  app "OpenEmu.app"
+
+  # Download and extract cores
+  postflight do
+    cores_path = "#{Dir.home}/Library/Application Support/OpenEmu"
+    cores_zip_path = "#{staged_path}/Cores.zip"
+    
+    # Create the Cores directory if it doesn't exist
+    system_command "/bin/mkdir", args: ["-p", cores_path]
+    
+    # Download Cores.zip
+    system_command "/usr/bin/curl", args: [
+      "-L", 
+      "https://github.com/Azyzraissi/OpenEmu/releases/download/2.4.1-ARM/Cores.zip",
+      "-o", cores_zip_path
+    ]
+    
+    # Extract Cores.zip to the Cores directory
+    system_command "/usr/bin/unzip", args: [
+      "-o",
+      cores_zip_path,
+      "-d", cores_path
+    ]
+    
+    # Clean up the downloaded zip file
+    system_command "/bin/rm", args: [cores_zip_path]
+  end
+
+  zap trash: [
+    "~/Library/Application Support/OpenEmu",
+    "~/Library/Caches/org.openemu.OpenEmu",
+    "~/Library/Preferences/org.openemu.OpenEmu.plist",
+    "~/Library/Saved Application State/org.openemu.OpenEmu.savedState"
+  ]
+end
